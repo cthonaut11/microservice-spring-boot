41ca2feec2c10d429af8312e065502ba6275ed11
cthonaut11 <lesn.vad@gmail.com>
2025-04-17 11:10:04 +0300

    improve service

--- vacation-pay-calculator/src/main/java/com/example/vacation_pay_calculator/service/VacationPayService.java
+++ vacation-pay-calculator/src/main/java/com/example/vacation_pay_calculator/service/VacationPayService.java
@@ -3,27 +3,19 @@ package com.example.vacation_pay_calculator.service;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.Calendar;
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
 public class VacationPayService {
 
-    private final int YEAR = Calendar.getInstance().get(Calendar.YEAR);
-
-    private final Set<LocalDate> HOLIDAYS = Set.of(
-
-            LocalDate.of(YEAR, 1, 1), LocalDate.of(YEAR, 1, 2), LocalDate.of(YEAR, 1, 3),
-            LocalDate.of(YEAR, 1, 4), LocalDate.of(YEAR, 1, 5), LocalDate.of(YEAR, 1, 6),
-            LocalDate.of(YEAR, 1, 7), LocalDate.of(YEAR, 1, 8),
-
-            LocalDate.of(YEAR, 2, 23), LocalDate.of(YEAR, 3, 8),
-            LocalDate.of(YEAR, 5, 1), LocalDate.of(YEAR, 5, 9),
-            LocalDate.of(YEAR, 6, 12), LocalDate.of(YEAR, 11, 4)
-    );
     public final double AVERAGE_DAYS_IN_MONTH = 29.3;
 
-    public double calculateVacationPay(int vacationDays, double averageSalary){
+    private double calculateAverageDailyEarnings(double averageSalary) {
+        return averageSalary / AVERAGE_DAYS_IN_MONTH;
+    }
+
+    public double calculateVacationPay(double averageSalary, int vacationDays){
         if(vacationDays <= 0 || averageSalary <= 0){
             throw new IllegalArgumentException("Количество дней и зарплата должны быть положительными");
         }
@@ -59,11 +51,30 @@ public class VacationPayService {
         return days;
     }
 
-    private double calculateAverageDailyEarnings(double averageSalary) {
-        return averageSalary / AVERAGE_DAYS_IN_MONTH;
+    private Set<LocalDate> getHolidays(int year) {
+        Set<LocalDate> holidays = new HashSet<>();
+        holidays.add(LocalDate.of(year, 1, 1));
+        holidays.add(LocalDate.of(year, 1, 2));
+        holidays.add(LocalDate.of(year, 1, 3));
+        holidays.add(LocalDate.of(year, 1, 4));
+        holidays.add(LocalDate.of(year, 1, 5));
+        holidays.add(LocalDate.of(year, 1, 6));
+        holidays.add(LocalDate.of(year, 1, 7));
+        holidays.add(LocalDate.of(year, 1, 8));
+
+        holidays.add(LocalDate.of(year, 2, 23));
+        holidays.add(LocalDate.of(year, 3, 8));
+        holidays.add(LocalDate.of(year, 5, 1));
+        holidays.add(LocalDate.of(year, 5, 9));
+        holidays.add(LocalDate.of(year, 6, 12));
+        holidays.add(LocalDate.of(year, 11, 4));
+
+        return holidays;
     }
 
     private boolean isHoliday(LocalDate date) {
-        return HOLIDAYS.contains(date);
+        return getHolidays(date.getYear()).contains(date) ||
+               (date.getYear() != date.plusYears(1).getYear() &&
+                getHolidays(date.getYear() + 1).contains(date));
     }
 }
