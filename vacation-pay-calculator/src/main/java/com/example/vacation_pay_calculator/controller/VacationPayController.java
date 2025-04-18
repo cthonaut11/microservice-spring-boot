5d3b9f6070fd1bdc31af880034b230bf695162ba
cthonaut11 <lesn.vad@gmail.com>
2025-04-18 12:02:19 +0300

    improve controller

--- vacation-pay-calculator/src/main/java/com/example/vacation_pay_calculator/controller/VacationPayController.java
+++ vacation-pay-calculator/src/main/java/com/example/vacation_pay_calculator/controller/VacationPayController.java
@@ -1,7 +1,9 @@
 package com.example.vacation_pay_calculator.controller;
 
+import com.example.vacation_pay_calculator.dto.VacationPayResponse;
 import com.example.vacation_pay_calculator.service.VacationPayService;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +20,35 @@ public class VacationPayController {
     }
 
     @GetMapping("/calculate")
-    public double calculateVacationPay(
+    public ResponseEntity<VacationPayResponse> calculateVacationPay(
             @RequestParam double averageSalary,
             @RequestParam(required = false) Integer vacationDays,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate){
-        if(vacationDays != null){
-            return vacationPayService.calculateVacationPay(averageSalary, vacationDays);
-        } else if (startDate != null && endDate != null) {
-            return vacationPayService.calculateExactVacationPay(averageSalary, startDate, endDate);
-        }else {
-            throw new IllegalArgumentException("Укажите количество дней либо дату начала отпуска с датой окончания");
+
+
+        try{
+
+            double result;
+            String message;
+
+            if(vacationDays != null){
+                result = vacationPayService.calculateVacationPay(averageSalary, vacationDays);
+                message = "Расчёт по количеству дней";
+            } else if (startDate != null && endDate != null) {
+                result = vacationPayService.calculateExactVacationPay(averageSalary, startDate, endDate);
+                message = "Расчёт по выбранным датам с учётом праздников";
+            }else {
+                throw new IllegalArgumentException("Укажите количество дней либо дату начала отпуска с датой окончания");
+            }
+
+            VacationPayResponse response = new VacationPayResponse(result, message);
+
+            return ResponseEntity.ok(response);
+
+        } catch (IllegalArgumentException e){
+            VacationPayResponse errorResponse = new VacationPayResponse(0.0, e.getMessage());
+            return ResponseEntity.badRequest().body(errorResponse);
         }
     }
 }
